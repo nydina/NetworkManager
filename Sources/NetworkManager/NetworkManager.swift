ae992e0b0a8ad3fc74d13c661d7b923b7b87f0f2
nydina <nydinarazafindratsira@gmail.com>
2023-03-30 15:49:26 +0200

    initial commit

--- Sources/NetworkManager/NetworkManager.swift
+++ Sources/NetworkManager/NetworkManager.swift
@@ -1,6 +1,21 @@
-public struct NetworkManager {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+@available(macOS 12.0, *)
+
+public struct NetworkManager {
+    func fetch<T: Codable>(from urlString: String) async throws -> T {
+        guard let url = URL(string: urlString) else  {
+            throw URLError(.badURL)
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+        
+        let result = try JSONDecoder().decode(T.self, from: data)
+        
+        return result
     }
 }
