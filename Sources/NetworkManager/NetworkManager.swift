edddeb055711e76348ba25716698f7315a7aefa1
nydina <nydinarazafindratsira@gmail.com>
2023-07-20 10:33:32 +0200

    Add, Update and delete methods

--- Sources/NetworkManager/NetworkManager.swift
+++ Sources/NetworkManager/NetworkManager.swift
@@ -2,6 +2,7 @@ import Foundation
 
 public struct NetworkManager {
     public init() {}
+    
     public func fetch<T: Codable>(from urlString: String) async throws -> T {
         guard let url = URL(string: urlString) else  {
             throw URLError(.badURL)
@@ -17,4 +18,78 @@ public struct NetworkManager {
         
         return result
     }
+    
+    public func add<T: Codable>(urlString: String) async throws -> T {
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        urlRequest.httpBody = try? JSONSerialization.data(withJSONObject: [:] as [String: Any])
+        
+        let (data, response) = try await URLSession.shared.upload(for: urlRequest, from: Data())
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              (200...299).contains(httpResponse.statusCode) else {
+            throw URLError(.badServerResponse)
+        }
+        
+        do {
+            let result = try JSONDecoder().decode(T.self, from: data)
+            return result
+        } catch {
+            throw error
+        }
+    }
+    
+    public func update<T: Codable>(urlString: String, requestData: [String: Any]) async throws -> T {
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "PUT"
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        do {
+            urlRequest.httpBody = try JSONSerialization.data(withJSONObject: requestData)
+        } catch {
+            throw error
+        }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: urlRequest)
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                throw URLError(.badServerResponse)
+            }
+            
+            let result = try JSONDecoder().decode(T.self, from: data)
+            return result
+        } catch {
+            throw error
+        }
+    }
+
+    public func delete(from urlString: String) async throws {
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "DELETE"
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let (_, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+    }
 }
+
